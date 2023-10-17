910d0006a0fdd8457eca8bdc3c305f97a86b3ef5
tophermurphy <tophermurphy@gmail.com>
2023-10-17 11:25:58 -0500

    lets switch to main

--- src/components/sections/Row1Column.tsx
+++ src/components/sections/Row1Column.tsx
@@ -2,17 +2,13 @@ import type { SectionRow } from "@/types/payload-types";
 import BlockRouter from "../BlockRouter";
 
 export interface SectionRow1Column extends SectionRow {
-    columns: "1"
+  columns: "1";
 }
 
-export default function Row1Column ({section}: {section: SectionRow1Column}) {
-
-    return(
-        <div>
-            <h3>Row 1 Col</h3>
-            { section.column_1 && 
-                <BlockRouter blocks={section.column_1} />
-            }
-        </div>
-    );
+export default function Row1Column({
+  section,
+}: {
+  section: SectionRow1Column;
+}) {
+  return section.col_1_blocks && <BlockRouter blocks={section.col_1_blocks} />;
 }
